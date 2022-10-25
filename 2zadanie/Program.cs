2399a4991bc839726e8cbf4dad7f69f1e8d033ac
sin9ikat <116688652+sin9ikat@users.noreply.github.com>
2022-10-26 02:43:21 +0300

    2zad
    1

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,14 @@
+﻿class Programm
+{
+    static int F(int x)  //Подключение метода
+    {
+        if (x%2==0) return x/2; //условный оператор
+        return x=0; //возвращаемое значение
+    }
+    static void Main() //точка входа в программу
+    {
+        Console.Write("Введите а="); //Подсказка
+        int a = int.Parse(Console.ReadLine());//Ввод
+        Console.WriteLine(F(a));  //вызов метода F
+    }
+}
